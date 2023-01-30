49161daaf40dbe68c306a49d9f4c398fabe5978d
leonardoyusuke <leoyuhirano@gmail.com>
2023-01-30 19:16:43 +0900

    end

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Carts from "./Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import SignIn from "../pages/SignInPage/SignIn";
+import SignUp from "../pages/SignUpPage/SignUp";
+import ProductsPage from "../pages/ProductsPage/ProductsPage";
+import Cart from "./Cart";
+import CheckOut from "./CheckOut"
+
 
 function App() {
+  const [user, setUser] = useState({});
+  const [precoTotal,setPrecoTotal] = useState("0")
   return (
-    <>
-    <Header />
-    <Carts />
-    <Footer />
-    </>
+    <UserContext.Provider value={{ user, setUser,precoTotal,setPrecoTotal }}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/home" element={<ProductsPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<CheckOut />} />
+      </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,69 +1,157 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import CheckOut from "./CheckOut"
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
-export default function Carts(){
+export default function Cart(){
+    const { precoTotal, setPrecoTotal } = useContext(UserContext);
+
+    const [numeroArtigo, setNumeroArtigo] = useState("")
+    const [itens,setItens]= useState("")
+    
 
  const mockArray = [
-        { name: "image01", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image02", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image03", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image04", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-      //  { name: "image05", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image06", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image07", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image08", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image09", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-       // { name: "image10", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
-        { name: "image11", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" }
+       { name: "placa de video", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" },
+        { name: "image02", tag: "common", price: "399,99", imageURL: "https://picsum.photos/140", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit" }
     ]
-    
-    let precoTotal 
-    function total(){
-        for(let i=0;i>mockArray.length;i++){
-            precoTotal = precoTotal + mockArray[i].price
-            console.log(mockArray[i].price)
-        }
-    }
-    
-    total()
-    return(
-        <>
-        <Bag > {mockArray.length}  itens  {mockArray.map((c) =><Map1> 
-                 <Img1 src={c.imageURL} />
-                 <Arrumacao>
-                 <Nome>
-                 {c.name}
-                 </Nome>
-                 <Descricao>
-                    {c.description} 
-                </Descricao> 
-                </Arrumacao>
-                <Preco>
-                R$  {c.price} 
-                </Preco>
-            </Map1>
-            
-            )}
+    const req = axios.get("http://localhost:5008/cart")
+    req.then((res)=>
+    setItens(res.data))
+    req.catch((error)=>
+    console.log(error))
 
-        </Bag>
-            TOTAL: {precoTotal}
+   
+    function somar(){
+        let preco = 0;
+        for(let i=0;i<mockArray.length;i++){
+           preco += parseFloat(mockArray[i].price.replace(',', '.'))
+        }
+        setPrecoTotal(preco)
+     }
+    
+    function quantidadeMais(){
+        const req = axios.patch("http://localhost:5008/cart/:name")
+        req.then((res)=>
+        setNumeroArtigo(res.data))
+    }
+    function quantidadeMenos(){
+        const req = axios.post("http://localhost:5008/cart/:name")
+        req.then((res)=>
+        setNumeroArtigo(res.data))
+    }
+
+    somar()
+    return(
+        <><Header />
+        <Bag>
+  {mockArray.length === 0 ? <Empty>Carrinho vazio</Empty> : mockArray.map((c) => (
+    <Map1>
+      <Img1 src={c.imageURL} />
+      <Arrumacao>
+        <Nome>{c.name}</Nome>
+        <Descricao>{c.description}</Descricao>
+        <Line />
+      </Arrumacao>
+      <BoxDireita>
+      <Quantidade>Quantidade: {numeroArtigo}
+        <Aumentar>
+            <div onClick={quantidadeMais}>
+      <ion-icon name="add-outline" ></ion-icon></div>
+      <div onClick={quantidadeMenos} >
+      <ion-icon name="remove-outline"></ion-icon></div>
+      </Aumentar>
+      </Quantidade>
+      <Preco>R$: {c.price}</Preco>
+      </BoxDireita>
+    </Map1>
+    ))}
+    {mockArray.length === 0 ? "":
+     <Preco1>TOTAL: R$: {precoTotal}</Preco1>
+    }</Bag>
+    {mockArray.length === 0 ? "": (
+  <Link to="/CheckOut">
+    <Checkout>
+      Finalizar compra
+    </Checkout>
+  </Link>
+)}
+        
+        <Footer />
         </>
     )
 }
+
+const Aumentar = styled.div`
+margin-left: 0px;
+margin-top: 5px;
+display: flex;
+justify-content: space-evenly;
+`
+
+const BoxDireita = styled.div`
+margin-left: -190px;
+
+`
+
+const Quantidade = styled.div`
+margin-top: 10px;
+display: flex;
+flex-direction: column;
+
+`
+
+const Line = styled.div`
+width: 350px;
+margin-left: -70px;
+border-bottom: 1px solid #000000;
+margin-top:40px;`
+ 
+const Empty = styled.div`
+text-align: center;
+`
+
+const Checkout = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 50px;
+width: 200px;
+border-radius: 10px;
+background-color: gray;
+margin: auto;
+
+`
+
+const Preco1 = styled.div`
+display:flex;
+justify-content: center;
+`
+
 const Preco = styled.div`
 position: absolute;
-right: 200px;
+margin-top: 40px;
+margin-bottom: 10px;
 `
 const Arrumacao = styled.div`
 `
 const Nome = styled.p`
 margin-bottom: 10px;
+margin-left: 10px;
 `
 
 const Descricao = styled.div`
 display: flex;
 flex-direction:wrap;
+width: 45%;
+margin-left: 10px;
 `
 const Map1 = styled.div`
+width: 100%;
 display: flex;
 margin-bottom:30px;
 margin-top:10px;`
@@ -77,4 +165,4 @@ const Bag = styled.div`
 width:100%;
 height: 100%;
 margin-left: 25px;
-background-color:whitesmoke;`
+position:relative`
