517b4b704f3bf86e12b86eaee8006986ec151f6a
DFCruzz <diegofcruzz@gmail.com>
2023-01-29 22:56:07 -0300

    feat: Paginas de produtos adicionadas

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import AuthContext from "../contexts/AuthContext";
 import SignIn from "../pages/SignInPage/SignIn";
 import ProductsPage from "../pages/ProductsPage/ProductsPage";
+import ProductsTypePage from "../pages/ProductsPage/ProductTypePage";
+import SingleProductPage from "../pages/ProductsPage/SingleProductPage";
 
 
 function App() {
@@ -10,13 +12,15 @@ function App() {
   return (
     <AuthContext.Provider value={{ token, setToken }}>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />}/>
-        <Route path="/home" element={<ProductsPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/home" element={<ProductsPage />} />
+          <Route path="/products/:type" element={<ProductsTypePage />} />
+          <Route path="product/:productId" element={<SingleProductPage />} />
+        </Routes>
       </BrowserRouter>
-    
-    
+
+
     </AuthContext.Provider>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Header = () => {
+
     return (
         <StyledHeader>
             <header>
